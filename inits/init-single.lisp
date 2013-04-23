1fdf4713c73101acb3f1671189ea73d84d268d00
akisute3 <akisute3@gmail.com>
2013-04-23 22:48:23 +0900

    シングルディスプレイ時の初期起動プログラムを設定

--- inits/init-single.lisp
+++ inits/init-single.lisp
@@ -9,7 +9,14 @@
 ;;; モードライン読み込み (起動後1度のみ)
 (defvar *complete-load-mode-line* nil)
 (unless *complete-load-mode-line*
-  (mode-line)
+  (run-shell-command "syndaemon -i 2 -d -t -k")
+  (run-shell-command "stalonetray")
+  (run-shell-command "nm-applet")
+  (run-shell-command "dropbox start")
+  (run-shell-command "gnome-sound-applet")
+  (run-shell-command "indicator-multiload")
+  (run-shell-command "classicmenu-indicator")
+  (run-shell-command "~/compile/batterymon-clone/batterymon")
   (setf *complete-load-mode-line* t))
 
 
